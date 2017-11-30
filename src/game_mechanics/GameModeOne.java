3f18aa7090fbfbdaac94310ae1a5eac0513661a7
Fabricio Melo <fabricio.melo@virtus.ufcg.edu.br>
2017-11-30 10:32:16 -0300

    Use this on game mode one

--- src/game_mechanics/GameModeOne.java
+++ src/game_mechanics/GameModeOne.java
@@ -12,22 +12,21 @@ public class GameModeOne implements GameMechanics {
 	private int hits;
 	private int mistakes;
 	private int attemptsPerWord;
-	private boolean canTryAgain;
 
 	GameModeOne() {
 		FactoryShuffler fs = new FactoryShuffler();
-		shuffler = fs.chooseShuffler();
-		totalWords = 0;
-		hits = 0;
-		mistakes = 0;
-    canTryAgain = true;
+		this.shuffler = fs.chooseShuffler();
+		this.totalWords = 0;
+		this.hits = 0;
+		this.mistakes = 0;
+		this.attemptsPerWord = 0;
 	}
 	
 	@Override
 	public String getScrambledWord() {
-		totalWords+= 1;
-		this.hiddenWord = shuffler.getWordOnBank();
-    String shuffledWord = shuffler.shufflerWord(this.hiddenWord);
+		this.totalWords+= 1;
+		this.hiddenWord = this.shuffler.getWordOnBank();
+    String shuffledWord = this.shuffler.shufflerWord(this.hiddenWord);
 		return shuffledWord.toUpperCase();
 	}
 
@@ -35,11 +34,11 @@ public class GameModeOne implements GameMechanics {
 	public String compareWords(String typedWord) {
 	  String response;
 	  if (this.hiddenWord.toUpperCase().equals(typedWord.toUpperCase())) {
-	    hits += 1;
+	    this.hits += 1;
 	    response = StringConstants.HIT_WORD;
     } else {
       response = StringConstants.MISSED_WORD;
-	    mistakes += 1;
+	    this.mistakes += 1;
     }
 		return response;
 	}
@@ -48,25 +47,23 @@ public class GameModeOne implements GameMechanics {
 	public boolean areEquals(String typedWord){
 	  if (this.hiddenWord.toUpperCase().equals(typedWord.toUpperCase())){
 	    this.hits+= 1;
-	    this.canTryAgain = false;
 	    this.attemptsPerWord = 0;
 	    return true;
     } else {
       this.mistakes+= 1;
       this.attemptsPerWord+= 1;
-      this.canTryAgain = this.attemptsPerWord < 3;
       return false;
     }
   }
 
 	@Override
 	public boolean continueGame() {
-		return totalWords < NumericalConstants.TOTAL_WORDS_PER_GAME;
+		return this.totalWords < NumericalConstants.TOTAL_WORDS_PER_GAME;
 	}
 
 	@Override
 	public boolean canTryAgain() {
-		return this.canTryAgain && this.attemptsPerWord < NumericalConstants.ATTEMPTS_PER_WORD;
+		return this.attemptsPerWord < NumericalConstants.ATTEMPTS_PER_WORD;
 	}
 
 	@Override
